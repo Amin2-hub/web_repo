4c421e66aafda27a188472cc6779954f44082643
Amin2-hub <wnstjd05@Naver.com>
2023-10-26 00:13:32 +0900

    10.26 집에서한거

--- helloWeb/src/main/webapp/todo/calendar.js
+++ helloWeb/src/main/webapp/todo/calendar.js
@@ -4,38 +4,52 @@
 //makeCalendar();
 
 
-function makeHead(month){
+function makeHead(month) {
     let day = ['일', '월', '화', '수', '목', '금', '토'];
 
     let head = '';
     head += '<thead><h3>' + month + '월</h3><tr>';
-    for(let prop in day){
+    for (let prop in day) {
         head += '<th>' + day[prop] + '</th>';
     }
     head += '</tr></thead>';
     return head;
 }
 
-function makeBody(){
-    let dates = [];
-    addDate = function(date){
-        for(let i=1; i<=31; i++){
-            this.dates.push(i);
-            console.log(i);
+function makeBody(month) {
+    let dates = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let cbody = '';
+
+    cbody += '<tbody>';
+    let sum = 1;
+    console.log(sum);
+    for (let i = 1; i <= (dates[month - 1] / 7 + 1); i++) {
+        cbody += '<tr>';
+        for (let j = 1; j <= 7; j++) {
+            if(sum <= dates[month-1]){
+                cbody += '<td>' + sum + '</td>';
+                sum++;
+            }
         }
+        cbody += '</tr>';
     }
+    cbody += '</tbody>';
+
+    return cbody;
 }
 
-function makeCalendar(month){
+function makeCalendar(month) {
     let cal = '';
     cal += '<table border="1">';
     cal += makeHead(month);
+    cal += makeBody(month);
+    console.log(makeBody(month));
     cal += '</table>';
     return cal;
 }
 
 document.getElementById('show').innerHTML += makeCalendar(10);
-makeBody();
+
 
 
 
